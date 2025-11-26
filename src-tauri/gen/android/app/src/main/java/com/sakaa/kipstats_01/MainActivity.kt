0c29cade3930079a804edc79175fd5ec8400eeb5
Sakaadar <79919443+Sakaadar@users.noreply.github.com>
2025-11-26 11:42:58 +0100

    first commit

---
+++ src-tauri/gen/android/app/src/main/java/com/sakaa/kipstats_01/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sakaa.kipstats_01
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
